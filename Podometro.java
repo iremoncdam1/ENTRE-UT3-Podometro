db2006af06d87791afb84b74f4c07c2c8e62db96
Iratxe Remón <iremoncdam1@gmail.com>
2021-10-11 11:14:26 +0200

    Añadidas constantes y atributos.

--- Podometro.java
+++ Podometro.java
@@ -3,11 +3,27 @@
  * acerca de los pasos, distancia, ..... que una persona
  * ha dado en una semana. 
  * 
- * @author    - pon aquí tu nombre - 
+ * @author    - Iratxe Remón - 
  */
 public class Podometro {
+    private final char HOMBRE = H;
+    private final char MUJER = M;
+    private final double ZANCADA_HOMBRE = 0.45;
+    private final double ZANCADA_MUJER = 0.41;
+    private final int SABADO = 6;
+    private final int DOMINGO = 7;
+    private String marca;
+    private int altura;
+    private char sexo;
+    private int longitudZancada;
+    private int totalPasosLaborables;
+    private int totalPasosSabado;
+    private int totalPasosDomingo;
+    private double totalDistanciaSemana;
+    private double totalDistanciaFinSemana;
+    private double tiempo;
+    private int caminatasNoche;
     
-
     /**
      * Inicializa el podómetro con la marca indicada por el parámetro.
      * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
